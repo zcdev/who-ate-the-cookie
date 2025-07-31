04a24c3aaec902b8674a0f73a0e5782c993b2a0c
zcdev <zcdev@hotmail.com>
2025-07-30 20:39:09 -0700

    chore: update metadata object
    Edit social preview info and relink Open Graphic assets for prod URL

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,29 @@
-import { Rubik } from "next/font/google";
-import '../styles/globals.css'
+import { Rubik } from 'next/font/google'
+import '../styles/globals.css';
 
 const rubik = Rubik({
   variable: "--font-rubik",
   subsets: ["latin"],
 });
 
-// src/app/character/page.js
 export const metadata = {
-  title: 'Who Ate the Cookie – Character',
+  title: 'Who Ate the Cookie?',
   description: 'Choose a character and find out who ate the cookie!',
+  url: 'https://whoatethecookie.fun/',
+  openGraph: {
+    images: {
+      url: 'https://whoatethecookie.fun/who-ate-the-cookie-og-img.png',
+      alt: 'Who Ate the Cookie?',
+    },
+    icon: 'https://whoatethecookie.fun/who-ate-the-cookie-favicon.ico',
+  },
+  twitter: {
+    images: {
+      url: 'https://whoatethecookie.fun/who-ate-the-cookie-og-img.png',
+      alt: 'Who Ate the Cookie?',
+    },
+    icon: 'https://whoatethecookie.fun/who-ate-the-cookie-favicon.ico',
+  }
 };
 
 export default function RootLayout({ children }) {
